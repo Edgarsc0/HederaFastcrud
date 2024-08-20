917d39d2b31e60464f60b4ea9595f1bfcd101821
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2024-08-20 01:58:00 -0600

    db

--- app/api/db/config.js
+++ app/api/db/config.js
@@ -1,11 +1,11 @@
 import mysql2 from "mysql2/promise";
 
 const pool = mysql2.createPool(({
-    host: "localhost",
+    host: "autorack.proxy.rlwy.net",
     user: "root",
-    database: "fastCrud",
-    password: "Hal02012()",
-    port: 3306
+    database: "fastcrud",
+    password: "VfqRyAJFaoEfrmGdWtmlABPAwlXPGuSc",
+    port: 51495
 }));
 
 const con = await pool.getConnection();
